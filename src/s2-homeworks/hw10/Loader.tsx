7806bb29ab3f2e11dfd075b3d1255e7eef1a178b
Julia <veluat@gmail.com>
2023-03-10 16:31:18 +0300

    HW10

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,6 @@
 import s from './Loader.module.css'
+import icon from './icon.gif'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => <div className={s.loader}>
+    <img src={icon} alt="loading icon"/>
+</div>
