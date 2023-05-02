2534c1b107a4a7546ad1299097debb84edb2fa96
Julia <veluat@gmail.com>
2023-05-02 23:05:51 +0300

    Homework api update

--- src/s2-homeworks/hw05/pages/JuniorPlus.tsx
+++ src/s2-homeworks/hw05/pages/JuniorPlus.tsx
@@ -4,7 +4,6 @@ import HW11 from "../../hw11/HW11";
 import HW12 from "../../hw12/HW12";
 import HW13 from "../../hw13/HW13";
 import HW14 from "../../hw14/HW14";
-// import HW15 from '../../hw15/HW15'
 
 function JuniorPlus() {
     return (
