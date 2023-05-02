6883a83f8aef186c72fb5e4c831dbf82f70e513f
Julia <veluat@gmail.com>
2023-05-02 23:06:06 +0300

    HW14

--- src/s2-homeworks/hw14/HW14.tsx
+++ src/s2-homeworks/hw14/HW14.tsx
@@ -35,17 +35,18 @@ const HW14 = () => {
         getTechs(value)
             .then((res) => {
                 // делает студент
-
-                // сохранить пришедшие данные
-
-                //
+                if (res) {
+                    setTechs(res.data.techs)
+                    // сохранить пришедшие данные
+                    setLoading(false)
+                }
             })
     }
 
     const onChangeText = (value: string) => {
         setFind(value)
         // делает студент
-
+        setSearchParams(value)
         // добавить/заменить значение в квери урла
         // setSearchParams(
 
